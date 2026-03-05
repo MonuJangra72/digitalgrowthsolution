617f986e9797f362691834c3dee3472aa918d3df
MonuJangra72 <Monu@digitalgrowthsolution.in>
2026-03-05 14:52:12 +0530

    Create lander.js

---
+++ src/pages/lander.js
@@ -0,0 +1,8 @@
+export default function Lander() {
+  return (
+    <div style={{padding:"40px", fontFamily:"Arial"}}>
+      <h1>Digital Growth Solution</h1>
+      <p>This is our landing page.</p>
+    </div>
+  );
+}
